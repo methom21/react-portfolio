4603f9ce41f36f592a31d0af09743899a47fc632
methom21 <mikey.thompson8@gmail.com>
2021-07-29 10:50:19 -0700

    added components

---
+++ src/components/Aboutme.js
@@ -0,0 +1,10 @@
+ function Aboutme () {
+     return (
+     <div>
+    <h1>About Me</h1>
+    <p>I was born and raised in Boulder, Colorado. Growing up I was always outside, playing sports, riding my 
+    bike, skateboard, Ripstick etc. In highschool i was still very active I played football, basketball, lacrosse and i ran track as well, after too many concussions and injuries i decided it was time to figure out more passions of mine that hadnt been figured out yet. That is where my interest for computers and videogames started. Once i started getting really into videogames i decided i wanted to build a gaming PC so i did. i watched hours of youtube videos watching people build their own PCs and eventually once all my parts came in the mail i was able to build mine as well. Once i started to mess around with my PC i got intersted in what was goiong on behind the scenes to make these video games and websites actually work. A friend of mine told me to look at HTML coding so i taught myself a little bit of HTML and got intrigued with coding and stumbled across the U of A Fullstack bootcamp. now i am less than two weeks from graduating and i am super excited to see where it takes me!  </p>
+    </div>
+     );
+};
+export default Aboutme;
