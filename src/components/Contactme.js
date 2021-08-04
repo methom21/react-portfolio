1b8e7f31dc09fb85b5cfab68d59b597862ab1d54
methom21 <mikey.thompson8@gmail.com>
2021-08-03 21:00:29 -0700

    added the hyperlinks to let the user go to my repos or active sites with the applications ive made and linked the icons in the contact me section

--- src/components/Contactme.js
+++ src/components/Contactme.js
@@ -8,7 +8,7 @@ export default function Contactme() {
             <div className='icon-container'>
             <a href="https://github.com/methom21"><img src="https://img.icons8.com/nolan/96/github.png"alt= 'github2Icon'/></a>
             <a href="https://www.linkedin.com/in/michael-thompson-7443bb20b/"><img src="https://img.icons8.com/nolan/96/linkedin.png"alt= 'linkedinIcon'/></a>
-            <a href=""><img src="https://img.icons8.com/nolan/96/email.png"alt= 'emailIcon'/></a>
+            <a href="mailto:mikey.thompson8@gmail.com"><img src="https://img.icons8.com/nolan/96/email.png"alt= 'emailIcon'/></a>
             </div>
             </div>
         
--- src/components/home.js
+++ src/components/home.js
@@ -12,10 +12,10 @@ export default function Home(){
   <img className="card-img-top" src= {myImg} alt="MyImg"></img>
   <div className="card-body">
     <p className="card-text">Below you can find some of my favorite applications that i have created, and i linked them directly to their corresponding github repository or the active site link for that application. If you would like to check out my other work just click on 'React Portfolio MT' and it will take you to the homepage of my github. </p>
-    <img className="card-img" src= {myApp1} alt="App1" href='https://radiant-scrubland-75939.herokuapp.com/'></img>
-    <img className="card-img" src= {myApp2} alt="App2" href='https://protected-shore-51969.herokuapp.com/'></img>
-    <img className="card-img" src= {myApp3} alt="App3" href='https://fitness-tracker-mt.herokuapp.com/'></img>
-    <img className="card-img" src= {myApp4} alt="App4" href='https://github.com/methom21/Project-2-MT-ZK-JA'></img>
+    <a href='https://radiant-scrubland-75939.herokuapp.com/'><img className="card-img" src= {myApp1} alt="App1"/></a>
+    <a href='https://protected-shore-51969.herokuapp.com/'><img className="card-img" src= {myApp2} alt="App2" /></a>
+    <a href='https://fitness-tracker-mt.herokuapp.com/'><img className="card-img" src= {myApp3} alt="App3" /></a>
+    <a href='https://github.com/methom21/Project-2-MT-ZK-JA'><img className="card-img" src= {myApp4} alt="App4" /></a>
   </div>
 </div>
     )
